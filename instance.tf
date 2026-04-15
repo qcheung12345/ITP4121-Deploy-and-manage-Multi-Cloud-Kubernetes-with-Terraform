6a4f4acfa364b64a0bad5daf02d0b25880da5be0
qcheung12345 <q489183@gmail.com>
2026-04-15 13:34:59 +0000

    deploying the gcp

---
+++ instance.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "terraform" {
+  project      = "itp4121-terraform-492511"
+  name         = "terraform"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
