40b64f4643888502f88736ebbab8d6f67ee11965
qcheung12345 <q489183@gmail.com>
2026-04-16 13:36:51 +0000

    modify gcp and azure main.tf setting, Add AKE 1 and 2

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -20,6 +20,10 @@ variable "aks_subnet_cidr" {
   type = string
 }
 
+variable "aks_subnet_cidr2" {
+  type = string
+}
+
 variable "node_count" {
   type = number
 }
@@ -35,10 +39,11 @@ variable "kubernetes_version" {
 }
 
 locals {
-  rg_name     = coalesce(var.resource_group_name, "${var.project_name}-azure-rg")
-  vnet_name   = "${var.project_name}-azure-vnet"
-  subnet_name = "${var.project_name}-azure-aks-subnet"
-  aks_name    = substr(replace("${var.project_name}-azure-aks", "_", "-"), 0, 63)
+  rg_name      = coalesce(var.resource_group_name, "${var.project_name}-azure-rg")
+  vnet_name    = "${var.project_name}-azure-vnet"
+  subnet_name  = "${var.project_name}-azure-aks-subnet"
+  subnet_name2 = "${var.project_name}-azure-aks-subnet-2"
+  aks_name     = substr(replace("${var.project_name}-azure-aks", "_", "-"), 0, 63)
 }
 
 resource "azurerm_resource_group" "this" {
@@ -60,6 +65,13 @@ resource "azurerm_subnet" "aks" {
   address_prefixes     = [var.aks_subnet_cidr]
 }
 
+resource "azurerm_subnet" "aks_secondary" {
+  name                 = local.subnet_name2
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.aks_subnet_cidr2]
+}
+
 resource "azurerm_kubernetes_cluster" "this" {
   name                = local.aks_name
   location            = azurerm_resource_group.this.location
@@ -69,10 +81,13 @@ resource "azurerm_kubernetes_cluster" "this" {
   kubernetes_version = var.kubernetes_version
 
   default_node_pool {
-    name           = "system"
-    node_count     = var.node_count
-    vm_size        = var.node_vm_size
-    vnet_subnet_id = azurerm_subnet.aks.id
+    name               = "system"
+    node_count         = var.node_count
+    vm_size            = var.node_vm_size
+    vnet_subnet_id     = azurerm_subnet.aks.id
+    auto_scaling_enabled = true
+    min_count          = 1
+    max_count          = 5
   }
 
   identity {
@@ -87,6 +102,18 @@ resource "azurerm_kubernetes_cluster" "this" {
   role_based_access_control_enabled = true
 }
 
+resource "azurerm_kubernetes_cluster_node_pool" "secondary" {
+  name                  = "secondary"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
+  vm_size               = var.node_vm_size
+  node_count            = 1
+  vnet_subnet_id        = azurerm_subnet.aks_secondary.id
+
+  auto_scaling_enabled = true
+  min_count            = 1
+  max_count            = 3
+}
+
 output "resource_group_name" {
   value = azurerm_resource_group.this.name
 }
@@ -97,4 +124,8 @@ output "cluster_name" {
 
 output "subnet_id" {
   value = azurerm_subnet.aks.id
+}
+
+output "subnet_id_secondary" {
+  value = azurerm_subnet.aks_secondary.id
 }
--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -22,6 +22,10 @@ variable "subnet_cidr" {
   type = string
 }
 
+variable "subnet_cidr2" {
+  type = string
+}
+
 variable "node_count" {
   type = number
 }
@@ -54,6 +58,13 @@ resource "google_compute_subnetwork" "this" {
   network       = google_compute_network.this.id
 }
 
+resource "google_compute_subnetwork" "secondary" {
+  name          = "${local.network_name}-secondary"
+  ip_cidr_range = var.subnet_cidr2
+  region        = var.region
+  network       = google_compute_network.this.id
+}
+
 resource "google_container_cluster" "this" {
   name     = local.cluster_name
   location = var.region
@@ -68,6 +79,20 @@ resource "google_container_cluster" "this" {
   deletion_protection = false
 
   ip_allocation_policy {}
+
+  cluster_autoscaling {
+    enabled = true
+    resource_limits {
+      resource_type = "cpu"
+      minimum       = 1
+      maximum       = 10
+    }
+    resource_limits {
+      resource_type = "memory"
+      minimum       = 1
+      maximum       = 20
+    }
+  }
 }
 
 resource "google_container_node_pool" "primary" {
@@ -93,4 +118,8 @@ output "cluster_name" {
 
 output "network_name" {
   value = google_compute_network.this.name
+}
+
+output "subnet_name_secondary" {
+  value = google_compute_subnetwork.secondary.name
 }
